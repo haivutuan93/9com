90ebbd8910d117df4762573ca5edac5dc3cc510d
hai.vt <hai.vt@samsung.com>
2018-07-23 10:31:07 +0700

    remove ajax in admin ajax

--- wp-content/plugins/madara-core/inc/ajax.php
+++ wp-content/plugins/madara-core/inc/ajax.php
@@ -3,9 +3,6 @@
 class WP_MANGA_AJAX {
 
 	public function __construct() {
-                //add list chapter in reading manga page
-                add_action('wp_ajax_wp-manga-list-chapter-reading', array( $this,'wp_manga_list_chapter_reading' ) );
-
 		// upload manga in backend
 		add_action('wp_ajax_wp-manga-upload-chapter', array( $this,'wp_manga_upload_chapter' ) );
 
@@ -106,31 +103,6 @@ class WP_MANGA_AJAX {
 
 	}
         
-        function wp_manga_list_chapter_reading() {
-            $post_id = $_GET[postID];
-            $cur_chap = $_GET[curChap];
-            global $wp_manga_chapter, $wp_manga_text_type, $wp_manga;
-            
-            $chapter  = $wp_manga_chapter->get_chapter_by_slug( $post_id, $cur_chap );
-            $all_chapters = madara_get_global_wp_manga_functions()->get_all_chapters($post_id);
-            $all_chaps = $all_chapters[$chapter['volume_id']][chapters];
-            
-            $args = array(
-				'cur_chap'  => $cur_chap,
-				'chapter'   => $chapter,
-				'all_chaps' => $all_chaps,
-				'position'  => 'header',
-			);
-            $outout = '';
-                        
-            ob_start();
-            $wp_manga_text_type->manga_nav_ajax( $args, $post_id );
-            $outout = ob_get_contents();
-            ob_end_clean();
-            
-            echo $outout;
-        }
-
 	function chapter_navigate_page(){
 
 		if( empty( $_GET['postID'] ) ){
